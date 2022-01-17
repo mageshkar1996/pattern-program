7a0289b3661cbd646de4c42d9c169823541516c3
Mageshkar <96710996+mageshkar1996@users.noreply.github.com>
2022-01-17 22:43:36 +0530

    Add files via upload
    5 
    10 15 
    20 25 30 
    35 40 45 50 
    55 60 65 70 75

---
+++ PatternMultiplyOf5.java
@@ -0,0 +1,16 @@
+public class PatternMultiplyOf5
+{
+public static void main(String[] args)
+{ int k=5;
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+           
+            System.out.print(k+" ");
+            k+=5;        
+        }
+    System.out.println();
+    }
+}
+}
