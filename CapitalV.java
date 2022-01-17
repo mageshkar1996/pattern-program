6140302a448b2a13017229d185a39e4cbb4fec52
Mageshkar <96710996+mageshkar1996@users.noreply.github.com>
2022-01-17 22:09:19 +0530

    Add files via upload
    +           + 
      +       +   
        +   +     
          +

---
+++ CapitalV.java
@@ -0,0 +1,17 @@
+public class CapitalV
+{
+public static void main(String[] args)
+{
+    for(int i=1;i<=4;i++)
+    {
+        for(int j=1;j<=7;j++)
+        {
+            if(i==j||j+i==8)
+            {System.out.print("+"+" ");}
+            else
+            {System.out.print(" "+" ");}
+        }
+    System.out.println();
+    }
+}
+}
